1718158e8b78c4480b8598d20ee98fd3b32cb753
Nila Dharmaraj <nila@dharmaraj.com>
2019-07-18 14:28:26 -0700

    all questions added, score added, almost completed

--- TriviaGame/TriviaGameViewController.swift
+++ TriviaGame/TriviaGameViewController.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class TriviaGameViewController: UIViewController {
     
-    var questions = ["Which singer’s real name is Stefani Joanne Angelina Germanotta?", " What is the name of Batman’s butler?", " What is Chandler’s last name in the sitcom Friends?", "Which pop album is the best selling of all time?", "Which nail grows fastest?", "Where is the smallest bone in the body?"]
+    var questions = ["Which singer’s real name is Stefani Joanne Angelina Germanotta?", " What is the name of Batman’s butler?", " What is Chandler’s last name in the sitcom Friends?", "Which pop album is the best selling of all time? Please say the artist!", "Which nail grows fastest?", "Where is the smallest bone in the body?", "Who cut Van Gough's ear?", " How many dots are there on two dice?",  "What language has the most words?", "What kind of animal is the jackrabbit?", "What kind of animal is a prairie dog?", "How long did the 100 years war last?", "What color is blueberry jam?", "Which fictional character was also known as Lord Greystoke?", "What kind of animal is the horned toad?"]
     var currQ = 0
-    var answers = ["Lady Gaga", "Alfred", "Bing", "Thriller by Micheal Jackson", "Middle", "Ear"]
+    var answers = ["Lady Gaga", "Alfred", "Bing", "Thriller by Michael Jackson", "Middle", "Ear", "Himself", "42", "English", "Hare", "A rodent", "116 years", "Purple", "Tarzan", "A lizard"]
+    var score = 0
     
     @IBOutlet weak var endOfGameMessage: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var userAnswerTextField: UITextField!
     @IBOutlet weak var validationLabel: UILabel!
-    
+    @IBOutlet weak var scoreLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         questionLabel.text = questions[currQ]
@@ -36,17 +37,22 @@ class TriviaGameViewController: UIViewController {
         
         if(userAnswer!.caseInsensitiveCompare(correctAnswer) == .orderedSame) {
             validationLabel.text = "Good job! You got the right answer!"
+                score += 1
         } else {
             validationLabel.text = "Sorry, that was incorrect. The correct answer is \(correctAnswer)"
         }
         currQ += 1
         if (currQ >= questions.count) {
-            endOfGameMessage.text = "Good game! Thanks for playing! :)"
+            if (score > questions.count/2) {
+                endOfGameMessage.text = "Good game! Thanks for playing! :)"
+            } else {
+                endOfGameMessage.text = "Aww! Better luck next time, pal. :)"
+            }
         } else {
             questionLabel.text = questions [currQ]
         }
         userAnswerTextField.text = ""
+        scoreLabel.text = "Score: \(String(score))"
     }
     
 }
-
